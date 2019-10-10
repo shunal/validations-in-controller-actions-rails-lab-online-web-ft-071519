12895226c8d175fe07f38f5a25ce4c2d6d9ea08a
Steven Hunal <stevehunal@gmail.com>
2019-10-10 18:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post.update(post_params)
+    
     if @post.valid?
-      @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
